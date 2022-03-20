c5a2db6d87993d6e0951541e537db644fd30482d
ec965 <22567131+ec965@users.noreply.github.com>
2022-03-19 19:20:28 -0700

    update migration

--- db/migrations/000005_create_tile_table.up.sql
+++ db/migrations/000005_create_tile_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS tiles(
   tile_id serial PRIMARY KEY,
   board_id INT REFERENCES boards(board_id) ON DELETE CASCADE,
   card_id INT REFERENCES cards(card_id) ON DELETE CASCADE,
-  row INT NOT NULL,
-  column INT NOT NULL,
-  complete BOOLEAN DEFAULT false,
+  row INT NOT NULL CHECK (row >= 0),
+  col INT NOT NULL CHECK (col >= 0),
+  complete BOOLEAN DEFAULT 'f'
 )
